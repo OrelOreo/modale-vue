149def74cdb7ece1fc87bf1829cb6bf0ae736992
OrelOreo <oreloreopro@gmail.com>
2022-11-15 11:43:13 +0100

    first commit

---
+++ src/components/ContenueModale.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+
+        <modale :revele="revele" :toggleModale="toggleModale"></modale>
+    <!--  props name: 'bonjour'/ bind la donnée revele && lie la methode toggleModale en props-->
+        <div v-on:click="toggleModale" class="btn-success">Ourve la modale</div>
+        <!-- Quand clique active la methode toggleModale -->
+
+    </div>
+    
+
+</template>
+
+<script>
+
+import FenetreModale from './FenetreModale.vue'
+
+export default {
+    name: 'ContenuModale',
+    data() {
+      return {
+        revele: false
+      } 
+    },
+    methods: {
+        toggleModale: function() {
+            this.revele = !this.revele
+        }
+    },
+    components: {
+        'modale': FenetreModale
+    }
+}
+
+</script>
+
+
+<style>
+
+.btn-success {
+    background-color: #4CAF50;
+    width: 140px;
+    color: white;
+    padding: 10px;
+    cursor:pointer;
+    margin: 0 auto;
+}
+
+</style>
---
+++ src/components/FenetreModale.vue
@@ -0,0 +1,72 @@
+<template>
+
+    <div class="bloc-modale" v-if="revele">
+        <!-- Si revele est true alors affiche -->
+
+        <div class="overlay" v-on:click="toggleModale"></div>
+        <!-- Si clique alors applique la methode toggleModale -->
+
+        <div class="modale">
+            <div class="btn-modale btn btn-danger" v-on:click="toggleModale">X</div>
+            <!-- pareil ici -->
+            <h2>Le contenu de la modale</h2>
+        </div>
+
+    </div>
+
+
+</template>
+
+<script>
+
+export default {
+    name:'FenetreModale',
+    props: ['revele','toggleModale']
+}
+
+</script>
+
+<style>
+
+.bloc-modale {
+    position: fixed;
+    top:0;
+    bottom:0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.overlay {
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top:0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+
+.modale {
+    border: 1px solid gray;
+    background-color: #f1f1f1;
+    color: #333;
+    padding: 30px;
+    position: fixed;
+}
+
+.btn {
+    background-color: red;
+    padding: 10px;
+    color: white;
+    font-weight: 600;
+    cursor: pointer;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+}
+
+</style>
